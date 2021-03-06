da0c952052552dc88f221f5d133c9d6d39f5c082
Gokulavasan21 <iamgokulavasan071@gmail.com>
2021-03-06 17:45:31 +0530

    Reading and pre-processing the data

---
+++ Problem Statement 2.py
@@ -0,0 +1,36 @@
+from random import seed
+from random import randrange
+from csv import reader
+from math import sqrt
+
+# Load a CSV file
+def load_csv(filename):
+    dataset = list()
+    with open("Social_Network_Ads.csv", 'r') as file:
+        csv_reader = reader(file)
+        for row in csv_reader:
+            if not row:
+                continue
+            dataset.append(row)
+    return dataset
+
+# Convert string column to integer
+def str_column_to_int(dataset, column):
+    class_values = [row[column] for row in dataset]
+    unique = set(class_values)
+    lookup = dict()
+    for i, value in enumerate(unique):
+        lookup[value] = i
+    for row in dataset:
+        row[column] = lookup[row[column]]
+    return lookup
+
+# Find the min and max values for each column
+def dataset_minmax(dataset):
+    minmax = list()
+    for i in range(len(dataset[0])):
+        col_values = [row[i] for row in dataset]
+        value_min = min(col_values)
+        value_max = max(col_values)
+        minmax.append([value_min, value_max])
+    return minmax
